a172c5eca3a1c56e415ef49c57e97323a8670240
lorella-marino <lorellamarino@gmail.com>
2025-07-10 18:26:08 +0200

    first commit

---
+++ src/main/java/it/lavoro/hera_db/HeraDbApplication.java
@@ -0,0 +1,13 @@
+package it.lavoro.hera_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeraDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeraDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lavoro/hera_db/HeraDbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lavoro.hera_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeraDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
